521c12006f57030aa07b4083fe9b5eec8717b6bb
Owlspec3086 <ethical.worker3@gmail.com>
2021-01-19 04:50:23 -0500

    working on sprint

---
+++ knexfile.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+    development: {
+      client: 'sqlite3',
+      connection: {
+        filename: './data/projects.sqlite3'
+      },
+      pool: {
+        afterCreate: (conn, done) => {
+          // runs after a connection is made to the sqlite engine
+          conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
+        }
+        },
+      useNullAsDefault:true
+    }
+}
